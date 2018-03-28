ea8153be900a85b1d5b281ae90cf59d4ca26e0bc
Pierre Marijon <pierre@marijon.fr>
2018-03-29 01:37:23 +0200

    Change project name and add MIT license

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -1,10 +1,33 @@
+/*
+Copyright (c) 2018 Pierre Marijon <pierre.marijon@inria.fr>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+/* standard include */
 #include <vector>
 #include <string>
 
-namespace chimdect {
+namespace yacrd {
 namespace utils {
 
 using name_len = std::pair<std::string, std::uint64_t>;
@@ -13,6 +36,6 @@ using interval = std::pair<std::uint64_t, std::uint64_t>;
 std::vector<std::string> split(const std::string& s, char delimiter);
 
 } // namespace utils
-} // namespace chimdect
+} // namespace yacrd
 
 #endif // UTILS_HPP
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,8 +1,32 @@
+/*
+Copyright (c) 2018 Pierre Marijon <pierre.marijon@inria.fr>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/* standard include */
 #include <fstream>
 
+/* project include */
 #include "parser.hpp"
 
-void chimdect::parser::paf(const std::string& filename, std::map<chimdect::utils::name_len, std::vector<chimdect::utils::interval> >* read2mapping)
+void yacrd::parser::paf(const std::string& filename, std::map<yacrd::utils::name_len, std::vector<yacrd::utils::interval> >* read2mapping)
 {
     std::uint64_t switch_val;
 
@@ -31,22 +55,22 @@ void chimdect::parser::paf(const std::string& filename, std::map<chimdect::utils
 
         if(read2mapping->count(std::make_pair(name_a, len_a)) == 0)
         {
-            read2mapping->emplace(std::make_pair(name_a, len_a), std::vector<chimdect::utils::interval>());
+            read2mapping->emplace(std::make_pair(name_a, len_a), std::vector<yacrd::utils::interval>());
         }
         read2mapping->at(std::make_pair(name_a, len_a)).push_back(std::make_pair(beg_a, end_a));
 
         if(read2mapping->count(std::make_pair(name_b, len_b)) == 0)
         {
-            read2mapping->emplace(std::make_pair(name_b, len_b), std::vector<chimdect::utils::interval>());
+            read2mapping->emplace(std::make_pair(name_b, len_b), std::vector<yacrd::utils::interval>());
         }
         read2mapping->at(std::make_pair(name_b, len_b)).push_back(std::make_pair(beg_b, end_b));
     }
 
 }
 
-void chimdect::parser::paf_line(const std::string& line, std::string* name_a, std::uint64_t* len_a, std::uint64_t* beg_a, std::uint64_t* end_a, std::string* name_b, std::uint64_t* len_b, std::uint64_t* beg_b, std::uint64_t* end_b)
+void yacrd::parser::paf_line(const std::string& line, std::string* name_a, std::uint64_t* len_a, std::uint64_t* beg_a, std::uint64_t* end_a, std::string* name_b, std::uint64_t* len_b, std::uint64_t* beg_b, std::uint64_t* end_b)
 {
-    std::vector<std::string> split_line = chimdect::utils::split(line, '\t');
+    std::vector<std::string> split_line = yacrd::utils::split(line, '\t');
 
     *name_a = split_line[0];
     *len_a = std::stoi(split_line[1]);
