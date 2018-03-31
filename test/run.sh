0430206e3ee510220bb8a60ed40b8e3901aa4e5a
Pierre Marijon <pierre@marijon.fr>
2018-03-31 18:13:43 +0200

    add test for filter options

--- test/run.sh
+++ test/run.sh
@@ -4,7 +4,7 @@ NC='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
-function run_test {
+function test_output {
     diff=$(./build/yacrd -i test/${1}.${2} | diff test/${1}.out -)
     if [ "${diff}" == "" ]
     then
@@ -15,7 +15,25 @@ function run_test {
     fi
 }
 
-run_test "no_coverage" "paf"
-run_test "2_extremity_1_middle" "paf"
-run_test "2_extremity_1_middle" "mhap"
-run_test "2_extremity_1_middle_position_switch" "paf"
+function test_filter {
+    ./build/yacrd -i test/${1}.${2} -f test/${1}.${3} -o test/${1}.filter.${3} > /dev/null
+    diff=$(diff test/${1}.filter.${3} test/${1}.filter.${3}.out)
+    if [ "${diff}" == "" ]
+    then
+	echo -e "${1}.${2} ${3}  : ${GREEN}PASSED${NC}"
+    else
+	echo -e "${1}.${2} ${3} : ${RED}FAILLED${NC}"
+	echo ${diff}
+    fi
+}
+
+test_output "no_coverage" "paf"
+test_output "2_extremity_1_middle" "paf"
+test_output "2_extremity_1_middle" "mhap"
+test_output "2_extremity_1_middle_position_switch" "paf"
+
+test_filter "2_extremity_1_middle" "paf" "paf"
+test_filter "2_extremity_1_middle" "paf" "mhap"
+test_filter "2_extremity_1_middle" "paf" "fasta"
+test_filter "2_extremity_1_middle" "paf" "fastq"
+
