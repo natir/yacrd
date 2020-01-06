bc3f2c5360d8adbdb7a3d948b753e5397d5b5ad5
Pierre Marijon <pmarijon@mpi-inf.mpg.de>
2020-01-06 23:03:06 +0100

    Add test for ondisk functionality

--- tests/run.rs
+++ tests/run.rs
@@ -88,7 +88,7 @@ mod tests {
             .stderr(Stdio::piped())
             .stdout(Stdio::piped())
             .spawn()
-            .expect("Could create yacrd subprocess");
+            .expect("Couldn't create yacrd subprocess");
 
         if !child.wait().expect("Error durring yacrd run").success() {
             let mut stdout = String::new();
@@ -103,6 +103,42 @@ mod tests {
         }
 
         diff_unorder("tests/truth.yacrd", "tests/result.yacrd");
+    }
+
+    #[test]
+    fn detection_ondisk() {
+        std::fs::create_dir(std::path::Path::new("tests/split/"))
+            .expect("We can't create temporary directory for ondisk test");
+
+        let mut child = Command::new("./target/debug/yacrd")
+            .args(&[
+                "-i",
+                "tests/reads.paf",
+                "-o",
+                "tests/result.ondisk.yacrd",
+                "-s",
+                "tests/split/",
+            ])
+            .stderr(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("Couldn't create yacrd subprocess");
+
+        if !child.wait().expect("Error durring yacrd run").success() {
+            let mut stdout = String::new();
+            let mut stderr = String::new();
+
+            child.stdout.unwrap().read_to_string(&mut stdout).unwrap();
+            child.stderr.unwrap().read_to_string(&mut stderr).unwrap();
+
+            println!("stdout: {}", stdout);
+            println!("stderr: {}", stderr);
+            panic!();
+        }
+
+        diff_unorder("tests/truth.yacrd", "tests/result.ondisk.yacrd");
+        std::fs::remove_dir_all(std::path::Path::new("tests/split/"))
+            .expect("We can't delete temporary directory of ondisk test");
     }
 
     #[test]
@@ -122,7 +158,7 @@ mod tests {
             .stderr(Stdio::piped())
             .stdout(Stdio::piped())
             .spawn()
-            .expect("Could create yacrd subprocess");
+            .expect("Couldn't create yacrd subprocess");
 
         if !child.wait().expect("Error durring yacrd run").success() {
             let mut stdout = String::new();
@@ -157,7 +193,7 @@ mod tests {
             .stderr(Stdio::piped())
             .stdout(Stdio::piped())
             .spawn()
-            .expect("Could create yacrd subprocess");
+            .expect("Couldn't create yacrd subprocess");
 
         if !child.wait().expect("Error durring yacrd run").success() {
             let mut stdout = String::new();
@@ -192,7 +228,7 @@ mod tests {
             .stderr(Stdio::piped())
             .stdout(Stdio::piped())
             .spawn()
-            .expect("Could create yacrd subprocess");
+            .expect("Couldn't create yacrd subprocess");
 
         if !child.wait().expect("Error durring yacrd run").success() {
             let mut stdout = String::new();
@@ -227,7 +263,7 @@ mod tests {
             .stderr(Stdio::piped())
             .stdout(Stdio::piped())
             .spawn()
-            .expect("Could create yacrd subprocess");
+            .expect("Couldn't create yacrd subprocess");
 
         if !child.wait().expect("Error durring yacrd run").success() {
             let mut stdout = String::new();
